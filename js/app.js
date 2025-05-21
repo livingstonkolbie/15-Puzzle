bb83dfcdedde814308583fe58b0083f6c1b3c450
livingstonkolbie <kolbie.livingston@mscoding.org>
2025-05-20 20:09:36 -0600

    got the basics down

---
+++ js/app.js
@@ -0,0 +1,5 @@
+
+let title = document.getElementById('title');
+title.addEventListener('click', () => {
+    // Welcome will ease in and out, then the instructions and Enter Game, then click enter to pull up the game board
+}) 
